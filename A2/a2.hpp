3fb5933c007a1c6f7443d3cdc5fb3f03b39771cb
Andrew Wilkinson <apwilkin@buffalo.edu>
2015-02-17 13:33:13 -0500

    Started work on A2 class in hpp file.

--- A2/a2.hpp
+++ A2/a2.hpp
@@ -5,9 +5,18 @@
 
 class A2 {
 	public:
+	//default constructor
 	
+	//copy constructor
+	A2(const A2& a2) {
+	}
 	
+	//(assignment) operator
+	A2& operator=(const A2& a2) {
+	}
+	//indexing operator
 	
+	~A2 { delete[] ptr_; } //destructor
 	
 	private:
 		int rows_;
