c22aaafae38a272536871c257f8329a1a7510ded
Andrew Wilkinson <apwilkin@buffalo.edu>
2015-03-19 20:33:13 -0400

    Started work on assignment.

--- A4/a4.hpp
+++ A4/a4.hpp
@@ -5,28 +5,41 @@
 
 int nrows_;
 int ncols_;
+int xpos_;
+int ypos_;
+int optimal_distance_ = -1;
+int current_distance_;
 Maze& maze_;
+
 // implement your function distance
 int distance(Maze& maze, int sx, int sy, int fx, int fy) {
+	if ((sx == fx) && (sy == fy)){return 0}
 	maze_ = maze;
 	nrows_ = maze.nrow();
 	ncols_ = maze.ncol();
+	xpos_ = sx;
+	ypos_ = sy;
+	if (
+
+	return optimal_distance_;
 } // distance
 
-bool is_above(int cx, int cy) {
-	maze_.is_visited(cx, cy - 1)
-}
-
-bool is_below(int cx, int cy) {
+void shortest_path(){
+	
 	
 }
 
-bool is_left(int cx, int cy) {
-	
+bool is_above_available(int cx, int cy) {
+	return (!(maze_.is_visited(cx, cy - 1))
 }
-
-bool is_right(int cx, int cy) {
-	
+bool is_below_available(int cx, int cy) {
+	return (!(maze_.is_visited(cx, cy + 1))
+}
+bool is_left_available(int cx, int cy) {
+	return (!(maze_.is_visited(cx - 1, cy))
+}
+bool is_right_available(int cx, int cy) {
+	return (!(maze_.is_visited(cx + 1, cy))
 }
 
 #endif // A4_HPP
