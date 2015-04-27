580ac97f7d46fab7a98d6b2c83f9f3bb16b9fe09
Andrew Wilkinson <apwilkin@buffalo.edu>
2015-04-27 09:48:43 -0400

    continuing work on a7

--- A7/a7.hpp
+++ A7/a7.hpp
@@ -2,24 +2,37 @@
 #define A7_HPP
 
 #include "symbol.hpp"
+#include <iostream>
+#include <queue>
 
 // IMPLEMENT YOUR FUNCTION huffman_tree
+
+class Compare_symbol {
+public:
+    bool operator()(bnode<symbol>* b1, bnode<symbol>* b2) {
+    	return b1.value()<b2.value();
+	}
+};
+
 template <typename Iter>
 bnode<symbol>* huffman_tree(Iter first, Iter last) {
-	std::vector<symbol> v1 (last - first);
-	std::vector<symbol> v2 (last - first);
+	std::priority_queue<bnode<symbol>, std::vector<symbol>, Compare_symbol> q;
+	int size = 0;
 	for (; first != last; first ++) {
-		v1.push_back(*first);
+		bnode<symbol> = 
+		q.push(*first);
+		size++;
 	}
-	v2 = v1;
-	int smallest;
-	int next_smallest;
+	std::cout<< "size" << size <<std::endl;
+	
 	
 }
 
 // IMPLEMENT YOUR FUNCTION release_tree
 void release_tree(bnode<symbol>* root){
-	
+	//go left (release tree left)
+	//go right (release tree right)
+	//delete root
 }
 
 #endif // A7_HPP
